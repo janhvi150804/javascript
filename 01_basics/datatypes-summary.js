85b91be529c3217ccea4bba2fea04f48caed9cc1
janhvi150804 <keeda4321@gmail.com>
2025-03-25 12:32:02 +0000

    container config done

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,31 @@
+// Primitive Types
+// 7 types => String , Number , Boolean , null , undefined , Symbol , BigInt
+
+// Referance (Non-Primitive)
+// Array , Objects , Functions
+
+const heros = ["shaktiman" , "naagraj" , "doga"];
+let myObj = {
+    name: "Radha",
+    age: 2,
+}
+
+const myfun = function(){
+    console.log("Hello world");
+}
+
+// TYPEOF 
+
+// Type of val => Result
+// umdefined => "undefined"
+// null => "object"
+// boolean => "boolean"
+// number => "number"
+// string => "string"
+// object => "object"
+// function => "function" aka object function
+// All non-primitive data types results into "object"
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
